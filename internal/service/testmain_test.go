c0e5d6ee5ec55b78b70327403d6f399f496eb336
Retno TC <retnotc@Retnos-MacBook-Neo.local>
2026-06-09 15:45:11 +0700

    fix: use separate test database (spendwise_test_db) for unit tests
    - Added TestMain in handler, service, repository packages
    - Tests no longer touch real spendwise_main_db
    - Created spendwise_test_db for isolated test data
    - Prevents accidental data loss during testing

---
+++ internal/service/testmain_test.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"os"
+	"testing"
+
+	"spendwise-ms/internal/config"
+	"spendwise-ms/internal/model"
+)
+
+func TestMain(m *testing.M) {
+	os.Setenv("DB_NAME", "spendwise_test_db")
+	config.DB = nil
+	cfg := config.Load()
+	config.InitDB(cfg)
+	config.AutoMigrate(&model.User{}, &model.Transaction{}, &model.Category{}, &model.SavingGoal{}, &model.GoalContribution{}, &model.Budget{})
+	code := m.Run()
+	os.Exit(code)
+}
